bd1f007a24db3768a26e008d04ecf73a32c084af
Bruno Lowagie <bruno@lowagie.com>
2014-01-29 15:32:09 +0000

    Third try to fix typo

---
+++ src/main/java/com/itextpdf/examples/FillFlattenMerge1.java
@@ -0,0 +1,61 @@
+package com.itextpdf.examples;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import javax.servlet.ServletContext;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.PdfCopy;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+public class FillFlattenMerge1 {
+
+    public byte[] manipulatePdf(ServletContext context, String template, String data) throws DocumentException, IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Document document = new Document();
+        // Using PdfCopy isn't a good idea in this use case.
+        // Take a look at FillFlattenMerge2 to find out how to do it right!
+        PdfCopy copy = new PdfCopy(document, output);
+        document.open();
+        ByteArrayOutputStream baos;
+        PdfReader reader;
+        PdfStamper stamper;
+        AcroFields fields;
+        StringTokenizer tokenizer;
+        BufferedReader br = new BufferedReader(new InputStreamReader(context.getResourceAsStream(data)));
+        String line = br.readLine();
+        while ((line = br.readLine()) != null) {
+            // create a PDF in memory
+            baos = new ByteArrayOutputStream();
+            reader = new PdfReader(context.getResourceAsStream(template));
+            stamper = new PdfStamper(reader, baos);
+            fields = stamper.getAcroFields();
+            tokenizer = new StringTokenizer(line, ";");
+            fields.setField("name", tokenizer.nextToken());
+            fields.setField("abbr", tokenizer.nextToken());
+            fields.setField("capital", tokenizer.nextToken());
+            fields.setField("city", tokenizer.nextToken());
+            fields.setField("population", tokenizer.nextToken());
+            fields.setField("surface", tokenizer.nextToken());
+            fields.setField("timezone1", tokenizer.nextToken());
+            fields.setField("timezone2", tokenizer.nextToken());
+            fields.setField("dst", tokenizer.nextToken());
+            stamper.setFormFlattening(true);
+            stamper.close();
+            reader.close();
+            // add the PDF to PdfCopy
+            reader = new PdfReader(baos.toByteArray());
+            copy.addDocument(reader);
+            reader.close();
+        }
+        br.close();
+        document.close();
+        return output.toByteArray();
+    }
+}
---
+++ src/main/java/com/itextpdf/examples/UnitedStates.java
@@ -0,0 +1,49 @@
+/**
+ * This example was written by Bruno Lowagie.
+ */
+package com.itextpdf.examples;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class UnitedStates {
+    
+    public byte[] createPdf(InputStream is) throws IOException, DocumentException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Document document = new Document(PageSize.A4.rotate());
+        PdfWriter.getInstance(document, baos);
+        document.open();
+        PdfPTable table = new PdfPTable(9);
+        table.setWidthPercentage(100);
+        table.setWidths(new int[]{4, 1, 3, 4, 3, 3, 3, 3, 1});
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line = br.readLine();
+        process(table, line);
+        table.setHeaderRows(1);
+        while ((line = br.readLine()) != null) {
+            process(table, line);
+        }
+        br.close();
+        document.add(table);
+        document.close();
+        return baos.toByteArray();
+    }
+    
+    public void process(PdfPTable table, String line) {
+        StringTokenizer tokenizer = new StringTokenizer(line, ";");
+        while (tokenizer.hasMoreTokens()) {
+            table.addCell(tokenizer.nextToken());
+        }
+    }
+}
